19b41512bf12a3f4319a92edcdd0c36f36638408
James D. Callahan III <darkenelf@gmail.com>
2021-04-18 08:57:46 -0500

    Add some more ignored currencies

--- Currencies/IgnoredCurrencyIDs.lua
+++ Currencies/IgnoredCurrencyIDs.lua
@@ -23,13 +23,29 @@ private.IgnoredCurrencyIDs = {
     125, -- Warsong Gulch Mark of Honor
     126, -- Wintergrasp Mark of Honor
     181, -- Honor Points DEPRECATED2
+    810, -- Black Iron Fragment
     830, -- n/a
     897, -- UNUSED
+    1154, -- Shadowy Coins
     1347, -- Legionfall Building - Personal Tracker - Mage Tower (Hidden)
     1349, -- Legionfall Building - Personal Tracker - Command Tower (Hidden)
     1350, -- Legionfall Building - Personal Tracker - Nether Tower (Hidden)
+    1592, -- Order of Embers
     1705, -- Warfronts - Personal Tracker - Iron in Chest (Hidden)
     1714, -- Warfronts - Personal Tracker - Wood in Chest (Hidden)
+    1738, -- Unshackled
+    1740, -- Rustbolt Resistance (Hidden)
+    1743, -- Fake Anima for Quest Tracking
+    1745, -- Nazjatar Ally - Neri Sharpfin
+    1746, -- Nazjatar Ally - Vim Brineheart
+    1747, -- Nazjatar Ally - Poen Gillbrack
+    1748, -- Nazjatar Ally - Bladesman Inowari
+    1749, -- Nazjatar Ally - Hunter Akana
+    1750, -- Nazjatar Ally - Farseer Ori
+    1769, -- Quest Experience (Standard, Hidden)
     1811, -- zzoldSanctum Architect
-    1812 -- zzoldSanctum Anima Weaver
+    1812, -- zzoldSanctum Anima Weaver
+    1850, --Choofa
+    1878, --Stitchmasters
+    1884 --The Avowed
 }
